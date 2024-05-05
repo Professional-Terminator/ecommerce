493ccc9dec700ed78b13ef452bd5e8a676f36923
SaqibOctave <sali@octavebytes.com>
2024-05-05 18:06:28 +0500

    First Commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { FeatureModule } from './Module/feature/feature.module';
+import { SharedModule } from './Module/shared/shared.module';
+import { AdminModule } from './Module/admin/admin.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FeatureModule,
+    SharedModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
