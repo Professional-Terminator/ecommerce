b2b0617a76fd09a0de550403db456920ed0ddcfc
saqibOctave <sali@octavebytes.com>
2024-08-19 14:58:08 +0500

    some updates

--- api/json-server.js
+++ api/json-server.js
@@ -1,9 +1,17 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Adjust the path if your db.json is located elsewhere
+const router = jsonServer.router('../db.json'); // Adjust the path if your db.json is located elsewhere
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+server.use(
+    jsonServer.rewriter({
+        "/*":"/$1",
+    })
+);
+server.listen(3000,()=>{
+    console.log("Server is running on 3000");
+})
 
 module.exports = server;
